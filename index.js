0a6cf3538a61357b3b7eccc820bfde0ba547dbd6
Kelvin <frednjogu22@gmail.com>
2024-03-30 16:21:14 +0300

    arrow functions

---
+++ index.js
@@ -0,0 +1,11 @@
+//has a function expression called divide
+let divide = function(){
+//   divide divides 2000 by 100  
+    return 2000 / 100;
+}
+//as an arrow function called square
+//square arrow function takes one parameter and multiplies it times itself
+    const square =(number) => number * number;
+//has an arrow function called add
+//add arrow function takes two parameters and adds them together
+    const add = (num1, num2) => num1 + num2;
